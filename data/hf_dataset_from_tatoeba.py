ab20860042cb33512df9824a1ff3fb729929a12f
stefanik12 <stefanik.mich@gmail.com>
2025-03-10 15:10:47 +0200

    WIP: HF dataset construction

--- data/hf_dataset_from_tatoeba.py
+++ data/hf_dataset_from_tatoeba.py
@@ -1,8 +1,9 @@
 import logging
 import os
-from typing import List, Tuple, Union
+from functools import partial
+from typing import List, Tuple, Union, Iterator, Dict
 
-from datasets import DatasetDict, Dataset
+from datasets import DatasetDict, Dataset, IterableDataset, Value, Features
 from tqdm import tqdm
 
 logger = logging.getLogger()
@@ -11,6 +12,7 @@ TATOEBA_DATA_DIR = 'data/example_data_dir'
 TARGET_HF_PATH = "michal-stefanik/toy_tatoeba_dataset"
 
 TARGET_LANG: Union[str, None] = None
+SOURCE_LANGS: Union[str, None] = None
 
 subdirs = os.listdir(TATOEBA_DATA_DIR)
 
@@ -23,12 +25,18 @@ if TARGET_LANG is not None:
     lang_subdirs = [l for l in lang_subdirs if TARGET_LANG in l]
     logger.warning("Subsetting to %s lang pairs containing %s", len(lang_subdirs), TARGET_LANG)
 
+if SOURCE_LANGS:
+    lang_subdirs = [l for l in lang_subdirs if any(src_l in l for src_l in SOURCE_LANGS.split(","))]
+    logger.warning("Subsetting to %s lang pairs containing %s", len(lang_subdirs), SOURCE_LANGS)
+
 
 def read_rows(dir: str, split: str) -> Tuple[List[str], List[str]]:
     path = os.path.join(dir, split)
     if os.path.exists(path+".src"):
         with open(path+".src") as src_f:
             with open(path+".trg") as trg_f:
+                # TODO: convert to stream in cases exceeding file size of cs-de (sucessfully processed on 512GB RAM):
+                #  -rw-rw-r-- 1 tiedeman project_462000447 3,1G okt  1  2023 train.src.gz
                 return [s.strip() for s in src_f.readlines()], [s.strip() for s in trg_f.readlines()]
 
     elif os.path.exists(path+".src.gz"):
@@ -43,27 +51,55 @@ def read_rows(dir: str, split: str) -> Tuple[List[str], List[str]]:
         return [], []
 
 
-for subdir in tqdm(lang_subdirs, desc="Uploading langs"):
-    if "eng" in subdir:
-        logger.warning("Skipping the upload of eng split for now")
+def generate_entries(dir: str, split: str) -> Union[None, Iterator[Dict[str, str]]]:
+    path = os.path.join(dir, split)
+    if os.path.exists(path+".src"):
+        with open(path+".src") as src_f:
+            with open(path+".trg") as trg_f:
+                # TODO: convert to stream in cases exceeding file size of cs-de (sucessfully processed on 512GB RAM):
+                #  -rw-rw-r-- 1 tiedeman project_462000447 3,1G okt  1  2023 train.src.gz
+                for src, tgt in zip(src_f.readlines(), trg_f.readlines()):
+                    yield {"source_text": src.strip(),
+                           "target_text": tgt.strip(),
+                           "source_lang": subdir.split("-")[0],
+                           "target_lang": subdir.split("-")[1]}
+    elif os.path.exists(path+".src.gz"):
+        import io
+        import gzip
 
-        continue
+        with io.TextIOWrapper(io.BufferedReader(gzip.open(path+".src.gz"))) as src_f:  # type: ignore
+            with io.TextIOWrapper(io.BufferedReader(gzip.open(path+".trg.gz"))) as trg_f:  # type: ignore
+                for src, tgt in zip(src_f.readlines(), trg_f.readlines()):
+                    yield {"source_text": src.strip(),
+                           "target_text": tgt.strip(),
+                           "source_lang": subdir.split("-")[0],
+                           "target_lang": subdir.split("-")[1]}
+    else:
+        logger.warning("Path %s not found, skipping", path)
+        return {}
+
+
+for subdir in tqdm(lang_subdirs, desc="Uploading langs"):
     logger.warning("Processing lang subdir %s", subdir)
-    # if TARGET_LANG is not None and TARGET_LANG not in subdir:
-    #     logger.warning("Skipping lang pair %s", subdir)
-    #     continue
 
     subdir_path = os.path.join(TATOEBA_DATA_DIR, subdir)
     lang_pair_dataset = DatasetDict()
     for split in ["train", "val", "dev", "test"]:
         if split == "dev":
             split = "val"
-        src, trg = read_rows(subdir_path, split)
-        if src and trg:
-            lang_pair_dataset[split] = Dataset.from_dict({"source_text": src, "target_text": trg,
-                                                          "source_lang": [subdir.split("-")[0]]*len(src),
-                                                          "target_lang": [subdir.split("-")[1]]*len(trg)
-                                                          })
+        # src, trg = read_rows(subdir_path, split)
+        # if src and trg:
+        #     lang_pair_dataset[split] = Dataset.from_dict({"source_text": src, "target_text": trg,
+        #                                                   "source_lang": [subdir.split("-")[0]]*len(src),
+        #                                                   "target_lang": [subdir.split("-")[1]]*len(trg)
+        #                                                   })
+        data_generator = generate_entries(subdir_path, split)
+        # if data_generator is not None:
+        # TODO: {TypeError}TypeError("cannot pickle 'generator' object")
+        #  https://huggingface.co/docs/datasets/en/create_dataset#from-python-dictionaries
+        features = Features({"source_text": Value("string"), "target_text": Value("string"),
+                    "source_lang": Value("string"), "target_lang": Value("string")})
+        lang_pair_dataset[split] = Dataset.from_generator(partial(generate_entries, dir=subdir_path, split=split), features=features)
 
     logger.warning("Pushing subset %s with splits %s into %s", subdir, lang_pair_dataset.keys(), TARGET_HF_PATH)
     lang_pair_dataset.push_to_hub(TARGET_HF_PATH, config_name=subdir)
